f8cf55a357a731bb47c40482dc56895ff9c955b3
unknown <melissikiss@gmail.com>
2023-05-17 16:40:37 -0700

    added description for services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -72,7 +72,7 @@ function Services() {
                                     <h3>Concrete Replacement</h3>
                                     <p>Concrete replacement involves removing and replacing deteriorated or damaged
                                         concrete surfaces, such as driveways, sidewalks, patios, or other concrete structures. We 
-                                        can assess and evaluate 
+                                        can assess and evaluate your project needs. 
                                     </p>
                                 </div>
                             </div>
@@ -89,9 +89,10 @@ function Services() {
                                 </div>
                                 <div className="col-md-8">
                                     <h3> Flatwork Specialists</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                        laboris nisi ut aliquip ex ea commodo consequat</p>
+                                    <p>Flatwork refers to concrete work that involves horizontal surfaces such as
+                                        driveways, sidewalks, patios, floors, and foundations. We promise precision, technique, 
+                                        and a beautiful finish. 
+                                    </p>
                                 </div>
                             </div>
 
@@ -105,9 +106,10 @@ function Services() {
                                 </div>
                                 <div className="col-md-8">
                                     <h3>Aggregate</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                        laboris nisi ut aliquip ex ea commodo consequat</p>
+                                    <p>Aggregate work typically involves the use of various types of aggregates, such as crushed
+                                        stone, gravel or sand, in concrete mixtures or for other construction purposes. We do
+                                        site preparation and grading, as well as placement and finishing. 
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -121,9 +123,11 @@ function Services() {
                                 </div>
                                 <div className="col-md-8">
                                     <h3>Stamp</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                        laboris nisi ut aliquip ex ea commodo consequat</p>
+                                    <p>Stamped concrete is a popular decorative technique that involves impressing 
+                                        patterns or textures onto freshly poured concrete, mimicking the appearance
+                                        of materials like stone, brick, or tile. We offer expertise, variety, and
+                                        attention to detail when performing this service. 
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -135,9 +139,11 @@ function Services() {
                                 </div>
                                 <div className="col-md-8">
                                     <h3>Broom Finish</h3>
-                                    <p className="services-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                        laboris nisi ut aliquip ex ea commodo consequat</p>
+                                    <p className="services-desc">Broom finishing is a method used to create 
+                                    a textured surface on concrete. It involves using a broom to create uniform grooves
+                                    or lines on the surface of freshly poured concrete. We use appropriate broom 
+                                    selection, deliver a consistent design, and work with precise timing and 
+                                    coordination.  </p>
                                 </div>
                             </div>
                         </div>
